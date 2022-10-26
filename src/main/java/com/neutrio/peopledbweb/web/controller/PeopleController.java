0b09962e966b6f0e16782642721c9d43c3a37dd6
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-26 10:28:04 +0900

    feat : updating people

--- src/main/java/com/neutrio/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/neutrio/peopledbweb/web/controller/PeopleController.java
@@ -54,4 +54,13 @@ public class PeopleController {
         return "redirect:people";
     }
 
+    @PostMapping(params="edit=true")
+    public String editPeople(@RequestParam Optional<List<Long>> selections, Model model){
+        if (selections.isPresent()) {
+            Optional<Person> person = personRepository.findById(selections.get().get(0));
+            model.addAttribute("person", person);
+        }
+        return "people";
+    }
+
 }
