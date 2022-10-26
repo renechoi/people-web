f008f536ec46bacf29d14ec726bf7aec6724c9b2
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-26 09:20:04 +0900

    refactor : a few numbers changed

--- src/main/java/com/neutrio/peopledbweb/data/PersonDataLoader.java
+++ src/main/java/com/neutrio/peopledbweb/data/PersonDataLoader.java
@@ -22,13 +22,13 @@ public class PersonDataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         if (personRepository.count() == 0) {
             List<Person> people = List.of(
-                    new Person(null, "Jake", "Snake", LocalDate.of(1950,1,1), "dummy@sample.com", new BigDecimal("5000")),
-                    new Person(null, "Sarah", "Smith", LocalDate.of(1960,1,1), "dummy@sample.com", new BigDecimal("6000") ),
-                    new Person(null, "Johny", "Jackson", LocalDate.of(1970,1,1), "dummy@sample.com", new BigDecimal("7000")),
-                    new Person(null, "Bobby", "Mark", LocalDate.of(1980,1,1), "dummy@sample.com", new BigDecimal("8000")),
+                    new Person(null, "Jake", "Snake", LocalDate.of(1950,2,1), "dummy@sample.com", new BigDecimal("5000")),
+                    new Person(null, "Sarah", "Smith", LocalDate.of(1960,5,1), "dummy@sample.com", new BigDecimal("6000") ),
+                    new Person(null, "Johny", "Jackson", LocalDate.of(1970,7,1), "dummy@sample.com", new BigDecimal("7000")),
+                    new Person(null, "Bobby", "Mark", LocalDate.of(1980,11,1), "dummy@sample.com", new BigDecimal("8000")),
                     new Person(null, "Jerry", "McGuire", LocalDate.of(1980,1,1), "dummy@sample.com", new BigDecimal("4000")),
-                    new Person(null, "Pete", "Jane", LocalDate.of(1980,1,1), "dummy@sample.com", new BigDecimal("3000")),
-                    new Person(null, "Kenny", "Kim", LocalDate.of(1980,1,1), "dummy@sample.com", new BigDecimal("2000"))
+                    new Person(null, "Pete", "Jane", LocalDate.of(1980,10,1), "dummy@sample.com", new BigDecimal("3000")),
+                    new Person(null, "Kenny", "Kim", LocalDate.of(1980,12,1), "dummy@sample.com", new BigDecimal("2000"))
             );
 
             personRepository.saveAll(people);
